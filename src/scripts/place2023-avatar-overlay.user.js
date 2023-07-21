62a0b4b8c544da8c642814365850177a054c520e
Vinzenz Rehn <vinzenz3000@gmx.de>
2023-07-21 19:40:12 +0200

    overlay update method test

--- src/scripts/place2023-avatar-overlay.user.js
+++ src/scripts/place2023-avatar-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place Avatar Overlay
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
@@ -25,7 +25,7 @@ if (window.top !== window.self) {
 }
 
 function updateImage() {
-    overlayImage.src = imageUrl + "?" + Date.now()
+    overlayImage.src = imageUrl + "#" + Date.now()
 }
 
-setInterval(function () {overlayImage.src = imageUrl + "?" + Date.now()}, 30000);
+setInterval(function () {overlayImage.src = imageUrl + "#" + Date.now()}, 30000);
